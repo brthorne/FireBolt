63580bfbe3264232182dfa159092065fc933f14c
drwiner <drwiner131@gmail.com>
2016-01-06 12:14:44 -0500

    added FunctionAction, not yet tested

---
+++ CinematicModel/CinematicModel/ModelStructure/FireBoltActions/FunctionAction.cs
@@ -0,0 +1,22 @@
+﻿using LN.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text;
+
+namespace CinematicModel.ModelStructure.FireBoltActions
+{
+
+
+    public class FunctionAction : FireBoltAction
+    {
+        public FunctionAction()
+        {
+        }
+
+        [XmlArray("args")]
+        [XmlArrayItem("arg")]
+        public List<Tuple<string, string, string>> tupleList { get; set; }
+    }
+}
