e2c567d1331baa9a1496f7eafc8d4157c87c5cb7
drwiner <drwiner131@gmail.com>
2016-01-07 16:43:15 -0500

    cleanup ingamefunction firebolt actions cript

--- FireBoltUnity/Assets/FireBolt/scripts/FireBoltActionImplementors/InGameFunction.cs
+++ FireBoltUnity/Assets/FireBolt/scripts/FireBoltActionImplementors/InGameFunction.cs
@@ -15,13 +15,7 @@ namespace Assets.scripts
         GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
         List<Tuple<string,string,string>> tupleList;
         Component ingamescript;
-        //MethodInfo mi;
-        //Type classType;
-        //object classInstance;
-        //MethodInfo initMi;
-        //MethodInfo undoMi;
-        //MethodInfo execMi;
-        //MethodInfo skipMi;
+
 
 
         public InGameFunction(float startTick, float endTick, string functionName, List<Tuple<string,string,string>> paramNames) :
@@ -29,39 +23,12 @@ namespace Assets.scripts
         {
             //Check that tuple list is okay, then...
            tupleList = paramNames;
+           ingamescript = gameController.GetComponent(functionName);
 
-
-            //find the functionName script.
-
-
-           // classType = Type.GetType(functionName);
-            // functionInstance = Activator.CreateInstance(classType);
-             
-             //classType = Type.GetType(functionName);
-             //classInstance = Activator.CreateInstance(classType);
-
-             ingamescript = gameController.GetComponent(functionName);
-             //Debug.Log(ingamescript.ToString());
-
-
-             //initMi = classType.GetMethod("Init");
-             //undoMi = classType.GetMethod("Undo");
-             //execMi = classType.GetMethod("Execute");
-             //skipMi = classType.GetMethod("Skip");
-             
-            //mi = this.GetType().GetMethod(functionName);
         }
 
         public override bool Init()
         {
-          //  mi.Invoke(this,tupleList);
-           // Activator.CreateInstance(classType);
-
-           // functionInstance.InvokeMember("Init", null);
-           // functionType.Invoke(Init, tupleList);
-
-            //initMi.Invoke(classInstance, new object[] { tupleList });
-            Debug.Log("WHEN");
             ingamescript.SendMessage("Init", tupleList);
             return true;
         }
@@ -69,19 +36,16 @@ namespace Assets.scripts
         public override void Undo()
 		{
             ingamescript.SendMessage("Undo");
-            //undoMi.Invoke(classInstance, null);
 		}
 
         public override void Skip()
         {
             ingamescript.SendMessage("Skip");
-            //skipMi.Invoke(classInstance, null);
         }
 
         public override void Execute(float currentTime) 
         {
             ingamescript.SendMessage("Execute");
-           // execMi.Invoke(classInstance, null);
 	    }
 
         public override void Stop()
