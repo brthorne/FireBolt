7eed5bd17079a1f5b8dce7f09fbf7a2257f77fc4
drwiner <drwiner131@gmail.com>
2016-01-19 14:12:29 -0500

    added documentation

--- FireBoltUnity/Assets/FireBolt/scripts/FireBoltActionImplementors/InGameFunction.cs
+++ FireBoltUnity/Assets/FireBolt/scripts/FireBoltActionImplementors/InGameFunction.cs
@@ -21,24 +21,36 @@ namespace Assets.scripts
         public InGameFunction(float startTick, float endTick, string functionName, List<Tuple<string,string,string>> paramNames) :
             base(startTick, endTick)
         {
+            //Unique objectName should also include paramNames
             objectName = startTick.ToString() + " " + endTick.ToString() + " " +  functionName;
+
+            //If the objectName is not in the registry of created Game Objects
             if (!ElPresidente.createdGameObjects.TryGet(objectName, out hostObject))
             {
+                //This prefab should have as component the functionName script, whatever functionName is. drawWhip_prefab has drawWhip script as component.
                 hostObject = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/FireBolt/Resources/" + functionName + "_prefab.prefab")) as GameObject;
                 //Debug.Log(hostObject.name);
+
+                //Attach to FireBolt object
                 GameObject fireBolt;
                 if (ElPresidente.createdGameObjects.TryGet("FireBolt", out fireBolt))
                 {
                     hostObject.transform.SetParent(fireBolt.transform);
                 }
+
+                //Add to ElPresidente registry of created Game Objects
                 ElPresidente.createdGameObjects.Add(objectName, hostObject);
+
+                //Record the parameters
                 tupleList = paramNames;
+                //Keep pointer to functionName script component
                 ingamescript = hostObject.GetComponent(functionName);
             }
             
 
         }
 
+        //Send Messages to functionName script
         public override bool Init()
         {
             ingamescript.SendMessage("Init", tupleList);
