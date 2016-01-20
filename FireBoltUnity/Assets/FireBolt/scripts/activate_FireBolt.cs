ffa327f520aabc65d1e677b640c6569c41642421
drwiner <drwiner131@gmail.com>
2016-01-20 14:12:12 -0500

    added activateFireBolt script and am testing new branch

---
+++ FireBoltUnity/Assets/FireBolt/scripts/activate_FireBolt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activate_FireBolt : MonoBehaviour {
+    public GameObject fireBolt;
+    public ElPresidente activate_FireBolt_script;
+	// Use this for initialization
+	void Start () {
+        //fireBolt = GameObject.FindGameObjectWithTag("FireBolt");
+        activate_FireBolt_script = fireBolt.GetComponent<ElPresidente>();
+        activate_FireBolt_script.Init(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
